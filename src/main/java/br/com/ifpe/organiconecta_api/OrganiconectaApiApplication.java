6f602873f22f18479d9a976b6b033accd2f6b43a
Wilson Barbosa <wilsonjunior_01@hotmail.com>
2024-12-07 00:36:53 -0300

    projeto configurado no spring initializr já com pacotes e dependencias

---
+++ src/main/java/br/com/ifpe/organiconecta_api/OrganiconectaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.organiconecta_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganiconectaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganiconectaApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/ifpe/organiconecta_api/util/entity/EntidadeAuditavel.java
@@ -0,0 +1,40 @@
+package br.com.ifpe.organiconecta_api.util.entity;
+import java.time.LocalDate;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Version;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+
+public abstract class EntidadeAuditavel extends EntidadeNegocio {
+       
+   @JsonIgnore
+   @Version
+   private Long versao;
+
+   @JsonIgnore
+   @CreatedDate
+   private LocalDate dataCriacao;
+
+   @JsonIgnore
+   @LastModifiedDate
+   private LocalDate dataUltimaModificacao;
+
+   @JsonIgnore
+   @Column
+   private Long criadoPor; // Id do usuário que o criou
+
+   @JsonIgnore
+   @Column
+   private Long ultimaModificacaoPor; // Id do usuário que fez a última alteração
+}
